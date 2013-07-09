49fbb4c9f92272ef6e362117b57821e92884cb96
Arlo Belshee <arlo.belshee@live.com>
2013-07-09 08:55:01 -0700

    An empty project or two. Runs one test. Also  integratess all the tools, including R# & NCrunch settings.

---
+++ Print4e/Print4e.Core/Class1.cs
@@ -0,0 +1,9 @@
+﻿// Class1.cs
+// 
+// Copyright 2013 Arlo Belshee (http:/github.com/arlobelshee).
+// All rights reserved. Usage as permitted by the LICENSE.txt file for this project.
+
+namespace Print4e.Core
+{
+	public class Class1 {}
+}
