5c8962d3ebc5f1b4d4028f19c8d0c9ef5bb6d9cc
Arlo Belshee <arlo.belshee@live.com>
2013-07-12 07:17:06 -0700

    Am now including some test data as an embedded resource and successfully reading it during a test.

---
+++ Print4e/Print4e.Core.Tests/Data/ParseCharacterFile.cs
@@ -0,0 +1,48 @@
+﻿// ParseCharacterFile.cs
+// 
+// Copyright 2013 Arlo Belshee (http:/github.com/arlobelshee).
+// All rights reserved. Usage as permitted by the LICENSE.txt file for this project.
+
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Print4e.Core.Tests.Data
+{
+	[TestFixture]
+	public class ParseCharacterFile
+	{
+		[Test]
+		public void ShouldFindTheName()
+		{
+			CharacterFile.FromXml(TestData.Kuri);
+		}
+	}
+
+	public class TestData
+	{
+		private static string _kuri;
+
+		public static string Kuri
+		{
+			get
+			{
+				if (_kuri == null)
+				{
+					var data = Assembly.GetExecutingAssembly()
+						.GetManifestResourceStream("Print4e.Core.Tests.Data.Kuri.dnd4e");
+					_kuri = data == null ? string.Empty : new StreamReader(data).ReadToEnd();
+				}
+				return _kuri;
+			}
+		}
+	}
+
+	public class CharacterFile
+	{
+		public static CharacterFile FromXml(string fileContents)
+		{
+			return new CharacterFile();
+		}
+	}
+}
