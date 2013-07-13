f2dd4e3bdeb8ac57d31c6e75875f9b56ee901d72
Arlo Belshee <arlo.belshee@live.com>
2013-07-13 11:09:33 -0700

    R# cleanup & file headeres.

--- Print4e/Print4e/Common/BooleanNegationConverter.cs
+++ Print4e/Print4e/Common/BooleanNegationConverter.cs
@@ -1,21 +1,26 @@
-﻿using System;
+﻿// BooleanNegationConverter.cs
+// 
+// Copyright 2013 Arlo Belshee (http:/github.com/arlobelshee).
+// All rights reserved. Usage as permitted by the LICENSE.txt file for this project.
+
+using System;
 using Windows.UI.Xaml.Data;
 
 namespace Print4e.Common
 {
-    /// <summary>
-    /// Value converter that translates true to false and vice versa.
-    /// </summary>
-    public sealed class BooleanNegationConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            return !(value is bool && (bool)value);
-        }
+	/// <summary>
+	///    Value converter that translates true to false and vice versa.
+	/// </summary>
+	public sealed class BooleanNegationConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			return !(value is bool && (bool) value);
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            return !(value is bool && (bool)value);
-        }
-    }
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			return !(value is bool && (bool) value);
+		}
+	}
 }
--- Print4e/Print4e/Common/BooleanToVisibilityConverter.cs
+++ Print4e/Print4e/Common/BooleanToVisibilityConverter.cs
@@ -1,32 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Graphics.Display;
-using Windows.UI.ViewManagement;
+﻿// BooleanToVisibilityConverter.cs
+// 
+// Copyright 2013 Arlo Belshee (http:/github.com/arlobelshee).
+// All rights reserved. Usage as permitted by the LICENSE.txt file for this project.
+
+using System;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 
 namespace Print4e.Common
 {
-    /// <summary>
-    /// Value converter that translates true to <see cref="Visibility.Visible"/> and false to
-    /// <see cref="Visibility.Collapsed"/>.
-    /// </summary>
-    public sealed class BooleanToVisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            return (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
-        }
+	/// <summary>
+	///    Value converter that translates true to <see cref="Visibility.Visible" /> and false to
+	///    <see cref="Visibility.Collapsed" />.
+	/// </summary>
+	public sealed class BooleanToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			return (value is bool && (bool) value) ? Visibility.Visible : Visibility.Collapsed;
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            return value is Visibility && (Visibility)value == Visibility.Visible;
-        }
-    }
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			return value is Visibility && (Visibility) value == Visibility.Visible;
+		}
+	}
 }
